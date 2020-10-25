037a74401757c3871b9ba6b704516404e349653a
grypesc <g.rypesc@wp.pl>
2020-10-25 17:02:32 +0100

    deep_q_learning agent first implementation

--- deep_q_learning_agent.py
+++ deep_q_learning_agent.py
@@ -1,6 +1,6 @@
-import random
-
+import cv2
 import gym
+import random
 import time
 
 import numpy as np
@@ -10,57 +10,64 @@ from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Dropout, Activation, Flatten
 from tensorflow.keras.optimizers import Adam
 
-MIN_REPLAY_MEMORY_SIZE = 1000
-MINIBATCH_SIZE = 32
+MIN_REPLAY_MEMORY_SIZE = 2048
+MINIBATCH_SIZE = 64
 DISCOUNT = 0.99
+UPDATE_TARGET_EVERY = 4
+NO_EPISODES = 20
 
 
 class DQNAgent:
-    def __init__(self, replay_memory_size=10000):
+    def __init__(self, replay_memory_size, states_shape, actions_shape, epsilon):
+        self.states_shape = states_shape
+        self.actions_shape = actions_shape
+        self.epsilon = epsilon
         self.model = self.create_model()
-
         # Target network
         self.target_model = self.create_model()
         self.target_model.set_weights(self.model.get_weights())
 
-        # An array with last n steps for training
         self.replay_memory = deque(maxlen=replay_memory_size)
-
-        # Custom tensorboard object
-        # self.tensorboard = ModifiedTensorBoard(log_dir="logs/{}-{}".format(MODEL_NAME, int(time.time())))
 
         # Used to count when to update target network with main network's weights
         self.target_update_counter = 0
 
-    def create_model(self, states_tensor_shape, actions_tensor_shape):
+    def create_model(self):
         model = Sequential()
 
-        model.add(Conv2D(128, (3, 3),
-                         input_shape=states_tensor_shape))  # states_space_shape = (10, 10, 3) a 10x10 RGB image.
+        model.add(Conv2D(64, (3, 3), strides=(2, 2),
+                         input_shape=self.states_shape))  # (80, 80, 1)
         model.add(Activation('relu'))
         model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Dropout(0.2))
+        # model.add(Dropout(0.2))
 
-        model.add(Conv2D(128, (3, 3)))
+        model.add(Conv2D(32, (3, 3), strides=(2, 2)))
         model.add(Activation('relu'))
         model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Dropout(0.2))
+        # model.add(Dropout(0.2))
 
         model.add(Flatten())  # flattens 3D feature maps to 1D feature vectors
-        model.add(Dense(64))
+        # model.add(Dense(32))
 
-        model.add(Dense(actions_tensor_shape, activation='linear'))  # ACTION_SPACE_SIZE = how many choices (5)
-        model.compile(loss="mse", optimizer=Adam(lr=0.001), metrics=['accuracy'])
+        model.add(Dense(self.actions_shape, activation='linear'))  # ACTION_SPACE_SIZE = how many choices (7)
+        model.compile(loss="mse", optimizer=Adam(lr=0.0005), metrics=['accuracy'])
+        model.summary()
         return model
 
-    def update_replay_memory(self, transition):
-        self.replay_memory.append(transition)
+    def update_replay_memory(self, previous_state, action, reward, current_state, done):
+        self.replay_memory.append([previous_state, action, reward, current_state, done])
+
+    def get_action(self, state):
+        if random.random() < self.epsilon:
+            return random.randint(0, self.actions_shape - 1)
+        return np.argmax(self.get_q_output(state))
 
     def get_q_output(self, state):
-        return self.model.predict(np.array(state).reshape(-1, *state.shape) / 255)[0]
+        state = np.reshape(state, (1, *state.shape))
+        prediction = self.model.predict(state) / 255
+        return prediction[0]
 
-    # Trains main network every step during episode
-    def train(self, terminal_state, step):
+    def train(self):
 
         # Start training only if certain number of samples is already saved
         if len(self.replay_memory) < MIN_REPLAY_MEMORY_SIZE:
@@ -68,43 +75,41 @@ class DQNAgent:
         # Get a minibatch of random samples from memory replay table
         minibatch = random.sample(self.replay_memory, MINIBATCH_SIZE)
 
-        # Get current states from minibatch, then query NN model for Q values
-        current_states = np.array([transition[0] for transition in minibatch]) / 255
-        current_qs_list = self.model.predict(current_states)
+        # Get previous states from minibatch, then calculate Q values
+        previous_states = np.array([mem_item[0] for mem_item in minibatch]) / 255
+        previous_q_values = self.model.predict(previous_states)
 
-        # Get future states from minibatch, then query NN model for Q values
         # When using target network, query it, otherwise main network should be queried
-        new_current_states = np.array([transition[3] for transition in minibatch]) / 255
-        future_qs_list = self.target_model.predict(new_current_states)
+        current_states = np.array([mem_item[3] for mem_item in minibatch]) / 255
+        current_q_values = self.target_model.predict(current_states)
 
         X = []
         y = []
 
         # Now we need to enumerate our batches
-        for index, (current_state, action, reward, new_current_state, done) in enumerate(minibatch):
+        for index, (previous_state, action, reward, current_state, done) in enumerate(minibatch):
 
             # If not a terminal state, get new q from future states, otherwise set it to 0
             # almost like with Q Learning, but we use just part of equation here
             if not done:
-                max_future_q = np.max(future_qs_list[index])
-                new_q = reward + DISCOUNT * max_future_q
+                max_current_q = np.max(current_q_values[index])
+                new_q = reward + DISCOUNT * max_current_q
             else:
                 new_q = reward
 
             # Update Q value for given state
-            current_qs = current_qs_list[index]
-            current_qs[action] = new_q
+            previous_qs = previous_q_values[index]
+            previous_qs[action] = new_q
 
             # And append to our training data
-            X.append(current_state)
-            y.append(current_qs)
+            X.append(previous_state)
+            y.append(previous_qs)
 
         # Fit on all samples as one batch, log only on terminal state
-        self.model.fit(np.array(X) / 255, np.array(y), batch_size=MINIBATCH_SIZE, verbose=0, shuffle=False)
+        self.model.fit(np.array(X) / 255, np.array(y), epochs=1, batch_size=MINIBATCH_SIZE, verbose=0, shuffle=False)
 
         # Update target network counter every episode
-        if terminal_state:
-            self.target_update_counter += 1
+        self.target_update_counter += 1
 
         # If counter reaches set value, update target network with weights of main network
         if self.target_update_counter > UPDATE_TARGET_EVERY:
@@ -113,29 +118,44 @@ class DQNAgent:
 
 
 def observation_to_state(observation):
-    # Simplify observation by reducing dimensions and turn in to a 160x160 state
+    # Simplify observation by reducing dimensions and turn in to a 80x80x1 state
     state = observation[75:235, :, :]
     state = np.sum(state, 2) / 3  # grayscale
+    state = cv2.resize(state, dsize=(80, 80), interpolation=cv2.INTER_LINEAR)
+    state = np.expand_dims(state, 2)
     return state
 
 
-if __name__ == "__main__":
+def main():
     env = gym.make('Assault-v0')
-    agent = DQNAgent()
-
-    for i_episode in range(20):
+    agent = DQNAgent(replay_memory_size=2048, states_shape=(80, 80, 1), actions_shape=6, epsilon=0.1)
+    for episode in range(NO_EPISODES):
         observation = env.reset()
-        time_step = 0
+        current_state = observation_to_state(observation)
+        time_step, previous_lives = 0, 4
         while True:
+            # if episode == NO_EPISODES - 1:
             env.render()
-            time.sleep(0.03)
-            action = np.argmax(agent.get_q_output(state))
-            observation, reward, done, info = env.step(action)
-            state
-            time_step += 1
+            # time.sleep(0.03)
+            action = agent.get_action(current_state)
+            previous_state = current_state
 
-            print(observation.shape, reward, done, info)
+            current_observation, reward, done, info = env.step(action + 1)
+            current_state = observation_to_state(current_observation)
+            time_step += 1
+            reward /= 21
+            if previous_lives > info["ale.lives"]:
+                previous_lives = info["ale.lives"]
+                reward = -5
+            agent.update_replay_memory(previous_state, action, reward, current_state, done)
+            print(episode, time_step)
+            agent.train()
+
             if done:
-                print("Episode finished after {} time steps".format(time_step + 1))
+                print("Episode finished after {} time steps".format(time_step))
                 break
     env.close()
+
+
+if __name__ == "__main__":
+    main()
