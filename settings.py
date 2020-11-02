9a359b7bf93b242404b2e3913f39c2e8eed0bed2
matkob <nologin90@gmail.com>
2020-11-02 10:35:33 +0100

    check if model dir exists

--- settings.py
+++ settings.py
@@ -1,3 +1,8 @@
 import os
+
+
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 MODELS_ROOT = os.path.join(PROJECT_ROOT, 'model')
+
+if not os.path.exists(MODELS_ROOT):
+    os.mkdir(MODELS_ROOT)
